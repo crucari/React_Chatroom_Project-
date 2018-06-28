75665dc0bf9dcb06162195656cc667d27aa880a1
Carina Cruz <lecruz@bu.edu>
2018-06-28 23:44:34 +0000

    app.js update

--- App.js
+++ App.js
@@ -1,4 +1,27 @@
 import React from 'react'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="app">
+                <RoomList />
+                <MessageList />
+                <SendMessageForm />
+                <NewRoomForm />
+            </div>
+        );
+    }
+}
+
+export default App
+______
+
+
+import React from 'react'
 import Chatkit from '@pusher/chatkit'
 import MessageList from './components/MessageList'
 import SendMessageForm from './components/SendMessageForm'
