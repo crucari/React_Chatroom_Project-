d2ec3e31a67bca39956826f1a2be40fe5b5c70ec
Carina Cruz <lecruz@bu.edu>
2018-07-16 23:42:08 +0000

    update

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ ______
 
 
 import React from 'react'
-import Chatkit from '@pusher/chatkit'
+import Chatkit from '@pusher/chatkit'  //chatkit
 import MessageList from './components/MessageList'
 import SendMessageForm from './components/SendMessageForm'
 import RoomList from './components/RoomList'
